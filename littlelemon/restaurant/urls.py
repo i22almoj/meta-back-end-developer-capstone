5fdabf6a7e87681d209aeb99346335cb66e5896e
Pepe Álvarez <hola@pepealvarez.dev>
2024-12-13 12:59:29 +0100

    Crear url y vista de Bookings

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -1,9 +1,13 @@
-from django.urls import path
-
+from django.urls import include, path
 from . import views
+from rest_framework.routers import DefaultRouter
+
+router: DefaultRouter = DefaultRouter()
+router.register(r'tables', views.BookingViewSet)
 
 urlpatterns = [
   path('', views.index, name='home'),
   path('menu/', views.MenuItemsView.as_view(), name='menu'),
   path('menu-item/<int:pk>/', views.SingleMenuItemView.as_view(), name='menu-detail'),
+  path('booking/', include(router.urls)),
 ]
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -19,3 +19,8 @@ class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [IsAuthenticated]
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
