cc68225ea26e0902d34403dee07ca2aca556919d
Pepe Álvarez <hola@pepealvarez.dev>
2024-12-14 12:14:33 +0100

    Cambiar nombre de app a LittleLemonAPI

--- littlelemon/LittleLemonAPI/serializers.py
+++ littlelemon/LittleLemonAPI/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-from restaurant.models import MenuItem, Booking
+from LittleLemonAPI.models import MenuItem, Booking
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
--- littlelemon/littlelemon/urls.py
+++ littlelemon/littlelemon/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('', lambda request: redirect('admin/')),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('api/', include('restaurant.urls')),
+    path('api/', include('LittleLemonAPI.urls')),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken'))
 ]
