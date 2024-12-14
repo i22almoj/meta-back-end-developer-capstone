bfc77caafefa9125f917cd16f04572b84db978b6
Pepe Álvarez <hola@pepealvarez.dev>
2024-12-14 11:58:27 +0100

    Añadir seguridad y autorización con djoser

--- littlelemon/littlelemon/urls.py
+++ littlelemon/littlelemon/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('api/', include('restaurant.urls')),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken'))
 ]
