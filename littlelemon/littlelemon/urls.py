53c861561432fa4773c1ea6950f161fd38363d89
Pepe Álvarez <hola@pepealvarez.dev>
2024-12-15 20:11:48 +0100

    Ordenar urls

--- littlelemon/littlelemon/urls.py
+++ littlelemon/littlelemon/urls.py
@@ -6,8 +6,8 @@ from django.shortcuts import redirect
 urlpatterns = [
     path('', lambda request: redirect('admin/')),
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('api/', include('LittleLemonAPI.urls')),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken'))
 ]
